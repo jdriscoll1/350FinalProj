f004aeac84a9f258995494b421e87eb9f94e03ba
Kelsey McMahon <kelsey.mcmahon@hotmail.com>
2022-06-10 16:32:56 -0700

    Adding tests to A1

--- task5/src/cs350s22/startup/Startup.java
+++ task5/src/cs350s22/startup/Startup.java
@@ -12,8 +12,23 @@ public class Startup {
 	
 	//Goal: create a linear actuator with some configurations 
 	public static void A1() throws Exception {
-		
-		startup.parse("CREATE ACTUATOR LINEAR act1 ACCELERATION LEADIN 0.1 LEADOUT -0.2 RELAX 0.3 VELOCITY LIMIT 5 VALUE MIN 1 MAX 20 INITIAL 2 JERK LIMIT 3"); 
+
+		//Create the mapper
+		startup.parse("CREATE MAPPER map1 EQUATION PASSTHROUGH");
+
+		//create a watchdog
+		startup.parse("CREATE WATCHDOG BAND watchdog1 MODE INSTANTANEOUS THRESHOLD LOW 0 HIGH 15");
+
+		//create the sensors (SPEED and POSITION)
+		startup.parse("CREATE SENSOR POSITION positionSensor1 WATCHDOGS watchdog1 MAPPER map1");
+		startup.parse("CREATE SENSOR SPEED speedSensor1 WATCHDOGS watchdog1 MAPPER map1");
+
+		//create the actuator with sensors
+		startup.parse("CREATE ACTUATOR LINEAR act1 SENSORS positionSensor1 speedSensor1 ACCELERATION LEADIN 0.1 LEADOUT -0.2 RELAX 0.3 VELOCITY LIMIT 5 VALUE MIN 1 MAX 20 INITIAL 2 JERK LIMIT 3");
+
+		//change the position to 15
+		startup.parse("SEND MESSAGE ID act1 POSITION REQUEST 15");
+
 		startup.parse("@EXIT");
 		
 	}
