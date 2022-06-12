366f93fb7f720c7c70b6d3c9626b6affbdecf95c
Kelsey McMahon <kelsey.mcmahon@hotmail.com>
2022-06-12 15:01:10 -0700

    adding tests A1 and A2

--- task5/src/cs350s22/startup/Startup.java
+++ task5/src/cs350s22/startup/Startup.java
@@ -11,43 +11,42 @@ public class Startup {
 	private final static Startup startup = new Startup();
 	private final A_ParserHelper _parserHelper = new ParserHelper();
 
-
 	//Goal: create a linear actuator with some configurations 
 	public static void A1() throws Exception {
 
-		//Create the mapper
-		//startup.parse("CREATE MAPPER map1 EQUATION PASSTHROUGH");
+		startup.parse("CREATE ACTUATOR LINEAR myActuator1 ACCELERATION LEADIN 0.1 LEADOUT -0.2 RELAX 0.3 VELOCITY LIMIT 5 VALUE MIN 1 MAX 20 INITIAL 2 JERK LIMIT 3");
 
-		//create a watchdog
+		startup.parse("BUILD NETWORK WITH COMPONENT myActuator1");
 
-	      startup.parse("CREATE REPORTER FREQUENCY myReporter1 NOTIFY ID cli FREQUENCY 3");
+		startup.parse("SEND MESSAGE PING");
 
-	      startup.parse("CREATE SENSOR POSITION mySensor1"); // add  REPORTERS myReporter1
-	      
-	      startup.parse("CREATE ACTUATOR LINEAR myActuator1 SENSOR mySensor1 ACCELERATION LEADIN 0.1 LEADOUT -0.2 RELAX 0.3 VELOCITY LIMIT 5 VALUE MIN 1 MAX 20 INITIAL 2 JERK LIMIT 3");
-	      
-	      startup.parse("BUILD NETWORK WITH COMPONENT myActuator1");
-	      
-	      startup.parse("SEND MESSAGE PING");
+	}
 
-	      startup.parse("SEND MESSAGE ID myActuator1 POSITION REPORT");
-	      
-	      //startup.parse("@CLOCK WAIT FOR 0");
-	      startup.parse("SEND MESSAGE ID myActuator1 POSITION REQUEST 15");
-	      startup.parse("@CLOCK WAIT FOR 0.5");
-	      startup.parse("SEND MESSAGE ID myActuator1 POSITION REPORT");
-	     
-	      startup.parse("@EXIT");    
-	      
-	   
+	public static void A2() throws Exception {
+
+		startup.parse("CREATE REPORTER FREQUENCY myReporter2 NOTIFY ID cli FREQUENCY 3");
+
+		startup.parse("CREATE SENSOR POSITION mySensor2");
+
+		startup.parse("CREATE ACTUATOR LINEAR myActuator2 SENSOR mySensor2 ACCELERATION LEADIN 0.1 LEADOUT -0.2 RELAX 0.3 VELOCITY LIMIT 5 VALUE MIN 1 MAX 20 INITIAL 2 JERK LIMIT 3");
+
+		startup.parse("BUILD NETWORK WITH COMPONENT myActuator2");
+
+		startup.parse("SEND MESSAGE PING");
+
+		startup.parse("SEND MESSAGE ID myActuator2 POSITION REPORT");
+
+		//startup.parse("@CLOCK WAIT FOR 0");
+		startup.parse("SEND MESSAGE ID myActuator2 POSITION REQUEST 15");
+		startup.parse("@CLOCK WAIT FOR 0.5");
+		startup.parse("SEND MESSAGE ID myActuator2 POSITION REPORT");
+
 	}
 	
 	public Startup() {
 		System.out.println("STARTUP");
 	}
 
-	
-	
 	public static void main(final String[] arguments) throws Exception {
 		
 		// this command must come first. The filenames do not matter here
@@ -55,6 +54,8 @@ public class Startup {
 
 		startup.parse("@CONFIGURE LOG a.txt DOT SEQUENCE b.txt NETWORK c.txt XML d.txt");
 		startup.A1();
+		startup.A2();
+		startup.parse("@EXIT");
 
 	}
 
